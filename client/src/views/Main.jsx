f2b743cd9f0897adc58e4dd305d0234d530f5d33
Vanessa Emerick <vavarooks123@gmail.com>
2023-01-04 08:04:37 -0800

    Fixed Profile

---
+++ client/src/views/Main.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Main = () => {
+
+    return (
+        <>
+
+            <div className='container d-block mx-auto w-50 my-3 p-3' >
+                <p>Welcome!</p>
+                <p>Look here!</p>
+                <p> Added </p>
+            </div>
+        </>
+    )
+
+}
+
+export default Main
