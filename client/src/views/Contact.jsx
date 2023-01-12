1827290e36a7eb8d5220eb5150789648630997e9
Vanessa Emerick <vavarooks123@gmail.com>
2023-01-11 21:13:27 -0800

    small add ons

---
+++ client/src/views/Contact.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Contact = () => {
+
+    return (
+        <>
+
+            <div className='container d-block mx-auto w-50 my-3 p-3' >
+                <p>Welcome!</p>
+                <p>Look here!</p>
+                <p> Added </p>
+                <p> More added to the page</p>
+            </div>
+        </>
+    )
+
+}
+
+export default Contact
