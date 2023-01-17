cbe65c0fb4cfc23d5189cbf270bd4baf189de13b
Vanessa Emerick <vavarooks123@gmail.com>
2023-01-17 08:22:22 -0800

    about update

--- client/src/views/About.jsx
+++ client/src/views/About.jsx
@@ -8,7 +8,11 @@ const About = () => {
 
             <div className='container d-block mx-auto w-50 my-3 p-3' >
                 <h1>About Me</h1>
-                <p>School I graduated from</p>
+                <p>School I graduated from:
+                    Saleisan Highschool Prepatory,
+                    UC Berkley Extension Program,
+                    Coding Dojo Fullstack.
+                </p>
                 <p>My Goals, My Dreams</p>
                 <p>My Skills: Python, Javascript, Java, MySQL, MongooseDB</p>
             </div>
